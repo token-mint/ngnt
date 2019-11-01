429c4541265742bfc957686152ef3647bab4b77c
Mayowa Tudonu <mtudonu85@gmail.com>
2019-11-01 14:10:20 +0100

    add test for initializer and minter

--- test/NGNT.test.js
+++ test/NGNT.test.js
@@ -25,27 +25,43 @@ contract('NGNT', function (accounts) {
         });
     });
 
-    it('contract should have correct initialization values', async function() {
-        const _tokenName = await ngntProxy.methods.name().call();
-        const _symbol = await ngntProxy.methods.symbol().call();
-        const _currency = await ngntProxy.methods.currency().call();
-        const _decimals = await ngntProxy.methods.decimals().call();
-        const _masterMinter = await ngntProxy.methods.masterMinter().call();
-        const _pauser = await ngntProxy.methods.pauser().call();
-        const _blacklister = await ngntProxy.methods.blacklister().call();
-        const _owner = await ngntProxy.methods.owner().call();
-        const _gsnFee = await ngntProxy.methods.gsnFee().call();
+    describe('contract should be properly initialized',  () => {
+        it('contract should have correct initialization values', async function() {
+            const _tokenName = await ngntProxy.methods.name().call();
+            const _symbol = await ngntProxy.methods.symbol().call();
+            const _currency = await ngntProxy.methods.currency().call();
+            const _decimals = await ngntProxy.methods.decimals().call();
+            const _masterMinter = await ngntProxy.methods.masterMinter().call();
+            const _pauser = await ngntProxy.methods.pauser().call();
+            const _blacklister = await ngntProxy.methods.blacklister().call();
+            const _owner = await ngntProxy.methods.owner().call();
+            const _gsnFee = await ngntProxy.methods.gsnFee().call();
 
-        assert.equal(_tokenName, tokenName);
-        assert.equal(_symbol,symbol);
-        assert.equal(_currency,currency);
-        assert.equal(_decimals,decimals);
-        assert.equal(_pauser,pauser);
-        assert.equal(_masterMinter,masterMinter);
-        assert.equal(_blacklister,blacklister);
-        assert.equal(_owner,owner);
-        assert.equal(_gsnFee,gsnFee);
+            assert.equal(_tokenName, tokenName);
+            assert.equal(_symbol,symbol);
+            assert.equal(_currency,currency);
+            assert.equal(_decimals,decimals);
+            assert.equal(_pauser,pauser);
+            assert.equal(_masterMinter,masterMinter);
+            assert.equal(_blacklister,blacklister);
+            assert.equal(_owner,owner);
+            assert.equal(_gsnFee,gsnFee);
+        });
     });
 
-    it('contract should h')
+    describe('contract should configure a minter', () => {
+        const nonMasterMinter = accounts[9];
+        const minter = accounts[3];
+        const minterAllowedAmount = 100;
+
+        //add test for events later
+        it('contract should not configure minter', async function() {
+            expect( async function (){
+                await ngntProxy.methods.configureMinter(minter, minterAllowedAmount).send({
+                    from: nonMasterMinter, gas: 50000, gasPrice: 1e6
+                }).to.throw();
+            })
+        });
+    });
+
 });
