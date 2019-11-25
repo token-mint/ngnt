72faf5609e4c5120708194a3fb65dbbc5ffa56d3
Mayowa Tudonu <mtudonu85@gmail.com>
2019-11-25 12:16:20 +0100

    add account signing key

--- test/utils.js
+++ test/utils.js
@@ -1,3 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const HDNode = require('ethers').utils.HDNode;
+const mnemonic = process.env.DEV_MNEMONIC;
+const masterNode = HDNode.fromMnemonic(mnemonic);
+
+
 const initializeFunctionParameters = [{
     "constant": true,
     "inputs": [],
@@ -14,6 +23,14 @@ const initializeFunctionParameters = [{
     }
 ];
 
+
+function getSigningKey(index){
+    const derivationPath = "m/44'/60'/0'/0/" + index.toString();
+    const addressNode = masterNode.derivePath(derivationPath);
+    return addressNode.privateKey;
+}
+
 module.exports = {
     initializeFunctionParameters,
+    getSigningKey
 };
