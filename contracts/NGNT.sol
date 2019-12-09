d19a51048c1b38d2d36dd9919e178577f127003d
Timi Ajiboye <trypio00@gmail.com>
2019-12-09 16:35:25 +0100

    fix: removed unlocked pragma

--- contracts/NGNT.sol
+++ contracts/NGNT.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.0;
 
 import '@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol';
 import "@openzeppelin/contracts-ethereum-package/contracts/GSN/GSNRecipient.sol";
